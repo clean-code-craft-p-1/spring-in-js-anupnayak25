a0dfb25045d1333400182ed7a9300197fdad5b31
anupnayak25 <mrnayak27@gmail.com>
2025-11-18 11:33:04 +0530

    added test cases and statics code

--- statistics.mjs
+++ statistics.mjs
@@ -1,5 +1,21 @@
-
-export function computeStatistics(numbers){
-    
-    // implement the computation of statistics here
+export function computeStatistics(numbers) {
+  if (!Array.isArray(numbers)) {
+    throw new TypeError("numbers must be an array");
+  }
+  if (numbers.length === 0) {
+    return { average: NaN, min: NaN, max: NaN };
+  }
+  let sum = 0;
+  let min = Infinity;
+  let max = -Infinity;
+  for (const num of numbers) {
+    const val = Number(num);
+    if (Number.isNaN(val)) {
+      return { average: NaN, min: NaN, max: NaN };
+    }
+    sum += val;
+    if (val < min) min = val;
+    if (val > max) max = val;
+  }
+  return { average: sum / numbers.length, min, max };
 }
--- test/statistics_test.mjs
+++ test/statistics_test.mjs
@@ -1,19 +1,43 @@
-import {computeStatistics} from '../statistics.mjs';
-import {expect} from 'chai';
+import { computeStatistics } from "../statistics.mjs";
+import { expect } from "chai";
 
-describe('Statistics', () => {
-    it('reports average, min, max', () => {
-        const computedStats = computeStatistics([98.6, 98.2, 97.8, 102.2]);
-        const epsilon = 0.001;
-        expect(computedStats.average).to.be.closeTo(99.2, epsilon);
-        expect(computedStats.min).to.be.closeTo(97.8, epsilon);
-        expect(computedStats.max).to.be.closeTo(102.2, epsilon);
-    });
-    it('reports NaN for empty input', () => {
-        const computedStats = computeStatistics([]);
-        // All fields of computedStats (average, max, min) must be
-        // NAN (not-a-number)
-        // Design the expect statement here.
-        // Use https://www.w3schools.com/jsref/jsref_isnan.asp
-    });
+describe("Statistics", () => {
+  it("reports average, min, max", () => {
+    const computedStats = computeStatistics([98.6, 98.2, 97.8, 102.2]);
+    const epsilon = 0.001;
+    expect(computedStats.average).to.be.closeTo(99.2, epsilon);
+    expect(computedStats.min).to.be.closeTo(97.8, epsilon);
+    expect(computedStats.max).to.be.closeTo(102.2, epsilon);
+  });
+  it("reports NaN for empty input", () => {
+    const computedStats = computeStatistics([]);
+    expect(Number.isNaN(computedStats.average)).to.be.true;
+    expect(Number.isNaN(computedStats.min)).to.be.true;
+    expect(Number.isNaN(computedStats.max)).to.be.true;
+  });
+
+  it("throws on non-array input", () => {
+    expect(() => computeStatistics("98.6")).to.throw(TypeError);
+  });
+
+  it("returns NaN triplet when any element is NaN", () => {
+    const computedStats = computeStatistics([98.6, NaN, 99.1]);
+    expect(Number.isNaN(computedStats.average)).to.be.true;
+    expect(Number.isNaN(computedStats.min)).to.be.true;
+    expect(Number.isNaN(computedStats.max)).to.be.true;
+  });
+
+  it("accepts numeric strings by coercion", () => {
+    const computedStats = computeStatistics(["98.6", "99.4"]);
+    expect(computedStats.min).to.equal(98.6);
+    expect(computedStats.max).to.equal(99.4);
+  });
+
+  it("returns NaN when any value is non-numeric", () => {
+    const computedStats = computeStatistics([98.6, "oops", 99.1]);
+    expect(Number.isNaN(computedStats.average)).to.be.true;
+    expect(Number.isNaN(computedStats.min)).to.be.true;
+    expect(Number.isNaN(computedStats.max)).to.be.true;
+  });
+  
 });
